c7605b343be29281c608945bbea85f993d59e33f
Goldenman676 <irontortoise360@gmail.com>
2023-11-03 13:28:19 -0400

    ADDING MOVE, HEAL, DAMAGE CLASSES
    adding move, heal, damage classes

---
+++ pokemonBattleImproved/pokemonBattleImproved/Healing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoveClass
+{
+    internal class Healing : Move
+    {
+        int type;
+        int healpower;
+    }
+}
