23738d85921d73aa04ef6c45fc4f7056be49b603
jhenderson2 <jace.henderson@snhu.edu>
2023-12-06 21:44:43 -0500

    v6

--- pokemonBattleImproved/pokemonBattleImproved/Properties/Resources.Designer.cs
+++ pokemonBattleImproved/pokemonBattleImproved/Properties/Resources.Designer.cs
@@ -476,6 +476,15 @@ namespace pokemonBattleImproved.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream forestbattle {
+            get {
+                return ResourceManager.GetStream("forestbattle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap Froakie_Button_120x125 {
